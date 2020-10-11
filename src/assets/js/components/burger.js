04e04dfde3b774c64cb9ea0c3c45781d5340defb
alexanderVLG <dominorog323@gmail.com>
2020-10-11 12:21:45 +0300

    testJob

---
+++ src/assets/js/components/burger.js
@@ -0,0 +1,14 @@
+
+// burger
+
+/* var addBurger = document.querySelector('.header__burger');
+var addMenu = document.querySelector('.header__menu');
+var bodyLock = document.querySelector('body');
+
+addBurger.addEventListener('click', addClass);
+
+function addClass() {
+  addBurger.classList.toggle('active');
+  addMenu.classList.toggle('active');
+  bodyLock.classList.toggle('lock');
+} */
